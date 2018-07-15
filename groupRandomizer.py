3d7bf8387a5b81afd50dffe7db9a6da3f88c5a8b
Dustin Armstrong <dieter@Dustins-MacBook-Pro.local>
2018-07-15 11:46:44 -0400

    refactoring

--- groupRandomizer.py
+++ groupRandomizer.py
@@ -1,17 +1,20 @@
 from random import shuffle
 
-group = [input('Enter player 1 '), input('Enter player 2 '), input('Enter player 3 '), input('Enter player 4 '), input('Enter player 5 '), input('Enter player 6 '), input('Enter player 7 '), input('Enter player 8 ')]
+playerAmount = int(input('How many players? '))
+group = []
+
+for i in range(playerAmount):
+    playerName = str(input('Enter Player Name: '.format(i+1)))
+    group.append(playerName)
 
 shuffle(group)
 
-print('Team 1 is: ')
-print(group[0])
-print(group[1])
-print(group[2])
-print(group[3])
+teamSize = int(playerAmount/2)
 
+team1 = group[:teamSize]
+team2 = group[teamSize:]
+
+print('Team 1 is: ')
+print(team1)
 print('Team 2 is: ')
-print(group[4])
-print(group[5])
-print(group[6])
-print(group[7])
+print(team2)
